06c2425d9cef12b4b40c784ee642480def6e0aba
L0RRIX <137098224+L0RRIX-1@users.noreply.github.com>
2024-11-07 09:40:49 +0100

    commit

--- components/main/molecules/GreetingTextWrapper.tsx
+++ components/main/molecules/GreetingTextWrapper.tsx
@@ -2,7 +2,7 @@ const GreetingTextWrapper = () => {
   return (
     <div className="flex text-3xl font-extrabold leading-9 tracking-tight sm:text-5xl sm:leading-10 md:text-6xl md:leading-14 pb-2">
       <h1 className="bg-gradient-to-r from-slate-500 to-slate-800 text-transparent bg-clip-text dark:from-gray-200 dark:to-slate-300 mb-2">
-      “The past is in your head. The future is in your hands.”
+        “The past is in your head. The future is in your hands.”
       </h1>
       <p>&nbsp;</p>
     </div>
