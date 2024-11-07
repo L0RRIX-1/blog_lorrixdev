f153b9dbe212accfd74cba9bd3bf37b04e6fff60
L0RRIX <137098224+L0RRIX-1@users.noreply.github.com>
2024-11-07 10:13:13 +0100

    Update Hits.tsx

--- components/common/atoms/Hits.tsx
+++ components/common/atoms/Hits.tsx
@@ -2,8 +2,8 @@
 const Hits = () => {
   return (
     <img
-      alt="Hits"
-      src="https://hits.sh/min71.dev.svg?view=today-total&style=flat-square&label=Visitors&extraCount=1&color=3b82f6"
+      alt=""
+      src=""
       style={{ width: '80px', height: 'auto', paddingTop: '1px' }}
       width={100}
       height={25}
